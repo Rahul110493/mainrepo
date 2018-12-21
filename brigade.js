74c4eb58a3395dd4776f92cf9690bbe26f9e886b
anupam <anupam.shukla@dentsuaegis.com>
2018-12-21 18:23:06 +0530

    added file

--- brigade.js
+++ brigade.js
@@ -30,10 +30,26 @@ events.on("push", function(e, project) {
   
   dockerBuild.run()
   
-  az login --service-principal -u $serviceuser -p $servicepass --tenant $servicetenant
-  az aks get-credentials --resource-group Myk8s --name Myk8s
-  kubectl set image deployment/nginx nginx=$mygcr/$gcrimage:latest
+ // Create a new job
+  var deploy = new Job("docker-deploy")
 
+  deploy.image = "microsoft/azure-cli:2.0.41"
+
+  deploy.env.SERVICE_USER = project.secrets.serviceuser 
+  deploy.env.SERVICE_PASS = project.secrets.servicepass
+  deploy.env.SERVICETENANT = project.secrets.servicetenant
+  deploy.env.GCR_REPONAME = project.secrets.mygcr
+  deploy.env.GCR_IMAGE = project.secrets.gcrimage
+
+
+  deploy.tasks = [
+   
+  "az login --service-principal -u $SERVICE_USER -p $SERVICE_PASS --tenant $SERVICETENANT",
+  "az aks get-credentials --resource-group Myk8s --name Myk8s",
+  "kubectl set image deployment/nginx nginx=$GCR_REPONAME/$GCR_IMAGE:latest"
+  ]
+
+  deploy.run()
   })  
 
 
